c738fce0a518d79e5650fc430d40807297f19b24
Kevin Wyllie <104919362+kwyllie24@users.noreply.github.com>
2022-05-18 17:40:54 -0500

    Update and rename SQL exploration to SQL exploration.sql

---
+++ SQL exploration.sql
@@ -0,0 +1,5 @@
+/*Data is located here: https://ourworldindata.org/covid-deaths
+
+Covid 19 Data Exploration 
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+*/
